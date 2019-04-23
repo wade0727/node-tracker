fdc088fcf8c4273c73ff8f849d6906c34a8166da
Matt <mdwade@gmail.com>
2019-04-23 11:40:14 -0500

    added style to cut complete checkbox in nodes show page

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -1,2 +1,10 @@
 module NodesHelper
+
+  def show_check_icon(boolean_value)
+    if boolean_value then
+      icon('fas', 'check', 'Complete!')
+    else
+      icon('fas', 'times', 'Incomplete!')
+    end
+  end
 end
